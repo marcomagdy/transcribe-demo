1336acf73406edf0e9433012bd9db7e57f26c7bb
Marco Magdy <mmagdy@gmail.com>
2019-06-05 12:05:59 -0700

    Cleanup for blog

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <aws/core/Aws.h>
-#include <aws/transcribestreaming/TranscribeStreamingServiceClient.h>
-#include <aws/transcribestreaming/model/StartStreamTranscriptionRequest.h>
-#include <aws/transcribestreaming/model/StartStreamTranscriptionHandler.h>
-#include <aws/core/utils/threading/Semaphore.h>
 #include <aws/core/utils/threading/Executor.h>
+#include <aws/core/utils/threading/Semaphore.h>
+#include <aws/transcribestreaming/TranscribeStreamingServiceClient.h>
+#include <aws/transcribestreaming/model/StartStreamTranscriptionHandler.h>
+#include <aws/transcribestreaming/model/StartStreamTranscriptionRequest.h>
 
-#include <fstream>
 #include <cstdio>
-#include <thread>
+#include <fstream>
 
 using namespace Aws;
 using namespace Aws::TranscribeStreamingService;
@@ -16,108 +15,63 @@ using namespace Aws::TranscribeStreamingService::Model;
 void pipe(Aws::IOStream& from, AudioStream& stream)
 {
     char buf[1024];
-    while(from)
-    {
+    while (from) {
         from.read(buf, sizeof(buf));
-        Aws::Vector<unsigned char> bits{buf, buf + from.gcount()};
+        Aws::Vector<unsigned char> bits { buf, buf + from.gcount() };
         AudioEvent event(std::move(bits));
         if (!stream.WriteAudioEvent(event))
             break;
     }
 }
 
-int StartRecording(Aws::IOStream& targetStream);
+int CaptureAudio(Aws::IOStream& targetStream);
 
-int main(int argc, char** argv)
+int SampleRate = 8000;
+
+int main()
 {
-    if (argc < 2) {
-        printf("needs a file argument.\n");
-        return 1;
-    }
-
     Aws::SDKOptions options;
     options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Trace;
     Aws::InitAPI(options);
     {
 
-        Client::ClientConfiguration config;
-        config.executor = Aws::MakeShared<Aws::Utils::Threading::PooledThreadExecutor>("", 1),
-        config.region = Aws::Region::US_WEST_2;
-        config.requestTimeoutMs = 1000;
-        config.disableExpectHeader = true;
-        // config.verifySSL = false;
-        // config.endpointOverride = "transcribe-streaming-beta.us-west-2.amazonaws.com";
-        // config.scheme = Aws::Http::Scheme::HTTP;
-        // config.endpointOverride = "172.22.86.246:8080";
-        TranscribeStreamingServiceClient client(config);
-
-        Aws::Utils::Threading::Semaphore sem(0, 1);
+        TranscribeStreamingServiceClient client;
+        Aws::Utils::Threading::Semaphore signaling(0 /*initialCount*/, 1 /*maxCount*/);
         StartStreamTranscriptionHandler handler;
         handler.SetTranscriptEventCallback([](const TranscriptEvent& ev) {
-                    for (auto && r : ev.GetTranscript().GetResults()) {
-                    if ( r.GetIsPartial() ) printf("[partial] ");
-                    else printf("[Final] ");
-                        for (auto && alt : r.GetAlternatives()) {
-                            printf("%s\n", alt.GetTranscript().c_str());
-                        }
-                    }
-                });
-
-        handler.SetOnErrorCallback([](const Aws::Client::AWSError<TranscribeStreamingServiceErrors>& ) {
-                printf("got an error\n");
-                });
+            for (auto&& r : ev.GetTranscript().GetResults()) {
+                if (r.GetIsPartial())
+                    printf("[partial] ");
+                else
+                    printf("[Final] ");
+                for (auto&& alt : r.GetAlternatives()) {
+                    printf("%s\n", alt.GetTranscript().c_str());
+                }
+            }
+        });
 
         StartStreamTranscriptionRequest request;
-        request.SetMediaSampleRateHertz(8000);
+        request.SetMediaSampleRateHertz(SampleRate);
         request.SetLanguageCode(LanguageCode::en_US);
         request.SetMediaEncoding(MediaEncoding::pcm);
         request.SetEventStreamHandler(handler);
 
-        auto OnStreamReady = [argv](AudioStream& stream)
-        {
-            if (strcmp(argv[1], "-") == 0)
-            {
-                StartRecording(stream);
-                printf("Done recording\n");
-            }
-            else
-            {
-                auto file = Aws::MakeShared<Aws::FStream>("", argv[1], std::ios_base::in | std::ios_base::binary);
-                if (!file || !*file) {
-                    printf("unable to open file.\n");
-                    return;
-                }
-                printf("Writing the audio data to the stream...\n");
-                pipe(*file, stream); // read from GStreamer and write to the stream
-            }
-
-            /*
-             * Signal to Transcribe that you're done streaming data.
-             * Note: this does not "flush" the data written all the way to the service. Instead, this flushes the data in
-             * the write buffer to the read buffer (wrapping and signing it in the process).
-             */
+        auto OnStreamReady = [](AudioStream& stream) {
+            CaptureAudio(stream);
             stream.flush();
-
-            /*
-             * Signals to the http client that you don't intend to send more data and that the request is now complete.
-             * This will effectively send any data left in the read buffer to the service and end the request.
-             * To send more data to the service you must create a new request and a new stream.
-             */
             stream.Close();
         };
 
-        auto OnResponseCallback = [&sem](const TranscribeStreamingServiceClient*,
-                const Model::StartStreamTranscriptionRequest&,
-                const Model::StartStreamTranscriptionOutcome&,
-                const std::shared_ptr<const Aws::Client::AsyncCallerContext>&)
-        {
-            printf("All done!\n");
-            sem.ReleaseAll();
+        auto OnResponseCallback
+            = [&signaling](const TranscribeStreamingServiceClient*, const Model::StartStreamTranscriptionRequest&,
+                  const Model::StartStreamTranscriptionOutcome&,
+                  const std::shared_ptr<const Aws::Client::AsyncCallerContext>&) {
+                  printf("All done!\n");
+                  signaling.Release();
+              };
 
-        };
-
-        client.StartStreamTranscriptionAsync(request, OnStreamReady, OnResponseCallback, nullptr/*context*/);
-        sem.WaitOne();
+        client.StartStreamTranscriptionAsync(request, OnStreamReady, OnResponseCallback, nullptr /*context*/);
+        signaling.WaitOne(); // prevent the application from exiting until we're done
     }
 
     Aws::ShutdownAPI(options);
--- record.cpp
+++ record.cpp
@@ -1,118 +1,91 @@
-#include <portaudio.h>
-#include <cstdio>
-#include <csignal>
 #include <aws/core/utils/memory/stl/AWSStreamFwd.h>
+#include <aws/core/utils/threading/Semaphore.h>
+#include <csignal>
+#include <cstdio>
+#include <portaudio.h>
 
-typedef int16_t SAMPLE;
-#define SAMPLE_RATE  (16000)
-#define NUM_CHANNELS (1)
-#define SAMPLE_SILENCE  (0)
+using SampleType = int16_t;
+extern int SampleRate;
+int Finished = paContinue;
+Aws::Utils::Threading::Semaphore pasignal(0 /*initialCount*/, 1 /*maxCount*/);
 
-int g_finished = paContinue;
-
-static int recordCallback( const void *inputBuffer, void *outputBuffer,
-        unsigned long framesPerBuffer,
-        const PaStreamCallbackTimeInfo* timeInfo,
-        PaStreamCallbackFlags statusFlags,
-        void *userData )
+static int AudioCaptureCallback(const void* inputBuffer, void* outputBuffer, unsigned long framesPerBuffer,
+    const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void* userData)
 {
-    auto stream= (Aws::IOStream*)userData;
-    const SAMPLE *rptr = (const SAMPLE*)inputBuffer;
+    auto stream = static_cast<Aws::IOStream*>(userData);
+    const SampleType* rptr = static_cast<const SampleType*>(inputBuffer);
 
-    (void) outputBuffer; /* Prevent unused variable warnings. */
-    (void) timeInfo;
-    (void) statusFlags;
+    (void)outputBuffer; // Prevent unused variable warnings
+    (void)timeInfo;
+    (void)statusFlags;
 
-    if( inputBuffer == nullptr )
-    {
-        for( unsigned long i=0; i<framesPerBuffer; i++ )
-        {
-            SAMPLE sample = SAMPLE_SILENCE;
-            stream->write((char*)&sample, sizeof(SAMPLE));
-            if (NUM_CHANNELS == 2) {
-                stream->write((char*)&sample, sizeof(SAMPLE));
-            }
-            else {
-            }
-            printf("got here \t ");
-        }
+    for (unsigned long i = 0; i < framesPerBuffer; i++) {
+        SampleType sample = static_cast<SampleType>(*rptr++);
+        stream->write(reinterpret_cast<char*>(&sample), sizeof(SampleType));
     }
-    else
-    {
-        for( unsigned long i=0; i < framesPerBuffer; i++ )
-        {
-            SAMPLE sample = static_cast<SAMPLE>(*rptr++);
-            stream->write((char*)&sample, sizeof(SAMPLE));
-            if (NUM_CHANNELS == 2) {
-                sample = static_cast<SAMPLE>(*rptr++);
-                stream->write((char*)&sample, sizeof(SAMPLE));
-            }
-        }
+
+    if (Finished == paComplete) {
+        pasignal.Release(); // signal the main thread to close the stream and exit
     }
-    return g_finished;
+
+    return Finished;
 }
 
-void intHandler(int) {
-    g_finished = paComplete;
-}
+void interruptHandler(int) { Finished = paComplete; }
 
-int StartRecording(Aws::IOStream& targetStream)
+int CaptureAudio(Aws::IOStream& targetStream)
 {
-
-    signal(SIGINT, intHandler);
-    PaStreamParameters  inputParameters;
-    PaStream*           stream;
-    PaError             err = paNoError;
+    signal(SIGINT, interruptHandler); // handle ctrl-c
+    PaStreamParameters inputParameters;
+    PaStream* stream;
+    PaError err = paNoError;
 
     err = Pa_Initialize();
-    if( err != paNoError ) {
-        fprintf(stderr,"Error: Failed to initialize PortAudio.\n");
+    if (err != paNoError) {
+        fprintf(stderr, "Error: Failed to initialize PortAudio.\n");
         return -1;
     }
 
-    inputParameters.device = Pa_GetDefaultInputDevice(); /* default input device */
+    inputParameters.device = Pa_GetDefaultInputDevice(); // default input device
     if (inputParameters.device == paNoDevice) {
-        fprintf(stderr,"Error: No default input device.\n");
+        fprintf(stderr, "Error: No default input device.\n");
         Pa_Terminate();
         return -1;
     }
-    inputParameters.channelCount = NUM_CHANNELS;
+
+    inputParameters.channelCount = 1;
     inputParameters.sampleFormat = paInt16;
-    inputParameters.suggestedLatency = Pa_GetDeviceInfo( inputParameters.device )->defaultHighInputLatency;
-    // inputParameters.suggestedLatency = 0.5;
+    inputParameters.suggestedLatency = Pa_GetDeviceInfo(inputParameters.device)->defaultHighInputLatency;
     inputParameters.hostApiSpecificStreamInfo = nullptr;
 
-    /* Record some audio. -------------------------------------------- */
-    err = Pa_OpenStream(
-            &stream,
-            &inputParameters,
-            nullptr,  /* &outputParameters, */
-            SAMPLE_RATE,
-            paFramesPerBufferUnspecified, // FRAMES_PER_BUFFER
-            paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-            recordCallback,
-            &targetStream);
+    // start the audio capture
+    err = Pa_OpenStream(&stream, &inputParameters, nullptr, /* &outputParameters, */
+        SampleRate, paFramesPerBufferUnspecified,
+        paClipOff, // we won't output out-of-range samples so don't bother clipping them.
+        AudioCaptureCallback, &targetStream);
 
-    if( err != paNoError ) {
+    if (err != paNoError) {
         fprintf(stderr, "Failed to open stream.\n");
         goto done;
     }
 
-    err = Pa_StartStream( stream );
-    if( err != paNoError ) {
+    err = Pa_StartStream(stream);
+    if (err != paNoError) {
         fprintf(stderr, "Failed to start stream.\n");
         goto done;
     }
-    printf("\n=== Now recording!! Speak into the microphone. ===\n"); fflush(stdout);
+    printf("=== Now recording!! Speak into the microphone. ===\n");
+    fflush(stdout);
 
-    while( ( err = Pa_IsStreamActive( stream ) ) == 1 )
-    {
-        Pa_Sleep(1000); // TODO: use thread signals rather than busy loop
+    if ((err = Pa_IsStreamActive(stream)) == 1) {
+        pasignal.WaitOne();
     }
-    if( err < 0 ) goto done;
 
-    err = Pa_CloseStream( stream );
-    if( err != paNoError ) goto done;
+    if (err < 0) {
+        goto done;
+    }
+
+    Pa_CloseStream(stream);
 
 done:
     Pa_Terminate();
